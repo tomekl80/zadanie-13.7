a5bbcd5cbecd195a5a4fa1de3e7bd02a1145c29a
Tomasz Lis <tomasz.lis@o2.pl>
2018-09-30 00:51:49 +0200

    Add code to read list of folders

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use sctict';
+
+var fs = require('fs');
+var colors = require('colors');
+
+fs.readdir('./', 'utf-8', function(err, files) {
+    console.log('Dane przed odczytem!'.blue);
+    console.log(files);
+    fs.writeFile('./lista.txt', files, function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
+        fs.readFile('./lista.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.blue)
+            console.log(data);
+        });
+    });
+});

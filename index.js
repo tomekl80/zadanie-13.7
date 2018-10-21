0b9654dbef741aa6fd38bdd122339bf68d4f74d3
Tomasz Lis <tomasz.lis@o2.pl>
2018-10-21 15:08:10 +0200

    Add files of excercise 13.8

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 var fs = require('fs');
 var colors = require('colors');
 
-fs.readFile('./test.txt', 'utf-8', function(err, data) {
-    console.log('Dane przed zapisem!'.blue);
-    console.log(data);
-    fs.appendFile('./test.txt', '\nA tak wyglądają po zapisie!', function(err) {
+fs.readdir('./', 'utf-8', function(err, files) {
+    console.log('Dane przed odczytem!'.blue);
+    console.log(files);
+    fs.writeFile('./lista.txt', (files.join(',\n')), function(err) {
         if (err) throw err;
         console.log('Zapisano!'.blue);
-        fs.readFile('./test.txt', 'utf-8', function(err, data) {
+        fs.readFile('./lista.txt', 'utf-8', function(err, data) {
             console.log('Dane po zapisie'.blue)
             console.log(data);
         });
